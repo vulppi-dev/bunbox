40bf2395022abc0d5b4c866d0f448b4b00e7c9b0
Renato Rodrigues <renato@vulppi.dev>
2025-09-02 07:51:09 -0300

    feat: add SDL function bindings for various components

---
+++ libs/sdl3/src/libs/functions/metal.ts
@@ -0,0 +1,8 @@
+import type { FFIFunction } from 'bun:ffi'
+
+export const METAL_BINDINGS = {
+  /**
+   * @description https://wiki.libsdl.org/SDL3/
+   */
+  //SDL_: {},
+} as const satisfies Record<string, FFIFunction>
---
+++ libs/sdl3/src/libs/functions/thread.ts
@@ -0,0 +1,8 @@
+import type { FFIFunction } from 'bun:ffi'
+
+export const THREAD_BINDINGS = {
+  /**
+   * @description https://wiki.libsdl.org/SDL3/
+   */
+  //SDL_: {},
+} as const satisfies Record<string, FFIFunction>
