8525e1021076375e16f7120688a3022e384e42e7
Renato Rodrigues <renato@vulppi.dev>
2025-09-03 15:50:36 -0300

    feat: update INIT_BINDINGS to include additional SDL functions and correct return types

--- libs/sdl3/src/libs/functions/init.ts
+++ libs/sdl3/src/libs/functions/init.ts
@@ -2,11 +2,46 @@ import type { FFIFunction } from 'bun:ffi'
 
 export const INIT_BINDINGS = {
   /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_GetAppMetadataProperty
+   */
+  SDL_GetAppMetadataProperty: { args: ['cstring'], returns: 'cstring' },
+  /**
    * @description https://wiki.libsdl.org/SDL3/SDL_Init
    */
-  SDL_Init: { args: ['u32'], returns: 'i32' },
+  SDL_Init: { args: ['u32'], returns: 'bool' },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_InitSubSystem
+   */
+  SDL_InitSubSystem: { args: ['u32'], returns: 'bool' },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_IsMainThread
+   */
+  SDL_IsMainThread: { args: [], returns: 'bool' },
   /**
    * @description https://wiki.libsdl.org/SDL3/SDL_Quit
    */
   SDL_Quit: { args: [], returns: 'void' },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_QuitSubSystem
+   */
+  SDL_QuitSubSystem: { args: ['u32'], returns: 'void' },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_RunOnMainThread
+   */
+  SDL_RunOnMainThread: { args: ['ptr', 'ptr', 'bool'], returns: 'bool' },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_SetAppMetadata
+   */
+  SDL_SetAppMetadata: {
+    args: ['cstring', 'cstring', 'cstring'],
+    returns: 'bool',
+  },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_SetAppMetadataProperty
+   */
+  SDL_SetAppMetadataProperty: { args: ['cstring', 'cstring'], returns: 'bool' },
+  /**
+   * @description https://wiki.libsdl.org/SDL3/SDL_WasInit
+   */
+  SDL_WasInit: { args: ['u32'], returns: 'u32' },
 } as const satisfies Record<string, FFIFunction>
