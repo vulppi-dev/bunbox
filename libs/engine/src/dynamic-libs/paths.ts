4cb39f861b9eb512679cb9ad11308125d1010f38
Renato Rodrigues <renato@vulppi.dev>
2025-11-03 23:30:52 -0300

    Remove BGFX dynamic library support and related structures
    - Deleted the loader.ts file which handled loading the BGFX library.
    - Removed structs and definitions related to BGFX from structs.ts.
    - Eliminated the BGFX library path definitions from paths.ts.

--- libs/engine/src/dynamic-libs/paths.ts
+++ libs/engine/src/dynamic-libs/paths.ts
@@ -34,21 +34,6 @@ const VK_LIBS: Record<string, any> = {
   },
 };
 
-const BGFX_LIBS: Record<string, any> = {
-  darwin: {
-    arm64: 'assets/arm64/darwin/libbgfx.dylib',
-    x64: 'assets/x64/darwin/libbgfx.dylib',
-  },
-  linux: {
-    arm64: 'assets/arm64/linux/libbgfx.so',
-    x64: 'assets/x64/linux/libbgfx.so',
-  },
-  win32: {
-    arm64: 'assets/arm64/win32/bgfx.dll',
-    x64: 'assets/x64/win32/bgfx.dll',
-  },
-};
-
 const FREE_TYPE_LIBS: Record<string, any> = {
   darwin: {
     arm64: 'assets/arm64/darwin/libfreetype.dylib',
@@ -79,10 +64,6 @@ export const GLFW_PATH = await getDynamicLibPath(
 
 // Don't need getDynamicLibPath for Vulkan, as it should be installed system-wide
 export const VK_PATH = VK_LIBS[process.platform]?.[process.arch];
-
-export const BGFX_PATH = await getDynamicLibPath(
-  BGFX_LIBS[process.platform]?.[process.arch],
-);
 
 export const FREE_TYPE_PATH = await getDynamicLibPath(
   FREE_TYPE_LIBS[process.platform]?.[process.arch],
